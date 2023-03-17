2250094262dd35b16fab8f5e747b63e0a5a8e438
Fatifonon <fatifnon0@example.com>
2023-03-17 13:10:59 -0700

    .

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,7 @@
  * followed by a newline, to standard error.
  * Return: Always 1
  */
-
 int main(void)
-
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
