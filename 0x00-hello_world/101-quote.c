0c8784a2a93e5489e15dce01f24cce0661aed787
Fatifonon <fatifnon0@example.com>
2023-03-17 12:31:50 -0700

    .

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdio.h>
 /**
- * main - A c program that prints a line to the standard error 
- * Return 1 (success)
+ * main - Entry point
+ *
+ * Return: Alwaus 0 (success)
 */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
-	return (1);
+Write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10--19"));
+return (1);
 }
