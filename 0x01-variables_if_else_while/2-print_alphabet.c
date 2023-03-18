3a8d9c0b2ae28bdd55da956d74df862368d0bfaa
Fatifonon <fatifnon0@example.com>
2023-03-17 18:45:20 -0700

    .

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercase,
+ * followed by a new line
+ * Return: always 0 (success)
+*/
+int main(void)
+{
+	char i;
+	
+	i= 'a';
+	while (i <= 'z')
+		putchar(i++);
+	putchar('\n');
+	return(0);
+}
