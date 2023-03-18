0f40cac3413d47d1800e63404fa709ef4294d06f
Fatifonon <fatifnon0@example.com>
2023-03-17 18:55:12 -0700

    .

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,11 +7,11 @@
 */
 int main(void)
 {
+
 	char i;
-	
-	i= 'a';
-	while (i <= 'z')
-		putchar(i++);
-	putchar('\n');
-	return(0);
+i = 'a';
+while (i <= 'z')
+putchar(i++)
+putchar('\n');
+return (0);
 }
