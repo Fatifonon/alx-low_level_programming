5edd32ba9c9d70ea2e17e14244bc68b55265040d
Fatifonon <fatifnon0@example.com>
2023-03-22 14:08:29 -0700

    .

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,45 +1,43 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * main - Prints the first 98 Fibonacci numbers, starting with
- * Return: default main 0.
+ * main - finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2
+ * followed by a new line
+ * Return: ALways 0 (Success)
  */
-
 int main(void)
 {
-	int i;
-	unsigned long f1 = 0, f2 = 1, res;
-	unsigned long f1_h1, f1_h2, f2_h1, f2_h2;
-	unsigned long h1, h2;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
-	for (i = 0; i < 92; i++)
+	j = 1;
+	k = 2;
+
+	printf("%lu", j);
+
+	for (i = 1; i < 91; i++)
 	{
-		res = f1 + f2;
-		printf("%lu, ", res);
-		f1 = f2;
-		f2 = res;
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	}
-	f1_h1 = f1 / 10000000000;
-	f2_h1 = f2 / 10000000000;
-	f1_h2 = f1 % 10000000000;
-	f2_h2 = f2 % 10000000000;
-	for (i = 93; i < 99; i++)
+
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
 	{
-		h1 = f1_h1 + f2_h1;
-		h2 = f1_h2 + f2_h2;
-		if  (f1_h2 + f2_h2 > 9999999999)
-		{
-			h1 += 1;
-			h2 %= 10000000000;
-		}
-		printf("%lu%lu", h1, h2);
-		if (i != 98)
-			printf(", ");
-		f1_h1 = f2_h1;
-		f1_h2 = f2_h2;
-		f2_h1 = h1;
-		f2_h2 = h2;
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
 	}
+
 	printf("\n");
+
 	return (0);
 }
