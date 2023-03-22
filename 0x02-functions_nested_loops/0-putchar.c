21488c08f870bb6c968bc8f927c14a3db26040e3
Fatifonon <fatifnon0@example.com>
2023-03-21 18:26:56 -0700

    .

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
-#include <main.h>
+#include "main.h"
+
 /**
- * main - Entry point
- * Description: 'prints _putchar to stdout'
+ * main - program that print _putchar
  * Return: always 0
- */
+*/
+
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char str[8] = "_putchar";
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
 	_putchar('\n');
 	return (0);
 }
