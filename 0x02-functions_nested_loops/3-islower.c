14503c439200cd754e2d0d2817d00a0cca2d7442
Fatifonon <fatifnon0@example.com>
2023-03-22 09:14:47 -0700

    .

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * int _islower(int c) - print 1 if c is in lowercase otherwise 
+ * Return: 0
+*/
+int _islower(int c)
+{
+	char letter = (1);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putcher(letter'\n');
+return:0
+}
+	
