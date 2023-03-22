1e31bb52329130c3223c3231eec08a2a37f3b27b
Fatifonon <fatifnon0@example.com>
2023-03-22 10:04:42 -0700

    .

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,12 +3,13 @@
 /**
  * _isalpha - check if a char alphabet
  * @c: is the car to be cheched
- * Return: 1 if char is lowercase or uppercase, otherwise 0.
+ *
+ * Return: 1 if char is lowercase or uppercase, 0 otherwise
 */
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z')|| (c >= 'A' && <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'Z'))
 		return (1);
 	else
 		return (0);
