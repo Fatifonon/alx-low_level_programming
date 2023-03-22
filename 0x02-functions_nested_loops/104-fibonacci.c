0e6e56bba830b572175b2004b561af586a16a07a
Fatifonon <fatifnon0@example.com>
2023-03-22 14:04:17 -0700

    .

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,31 +1,45 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-  * main - finds and prints the sum of the even=valued terms
-  * followed by a new line
-  * Return: Always 0 (Success)
-  */
+ * main - Prints the first 98 Fibonacci numbers, starting with
+ * Return: default main 0.
+ */
+
 int main(void)
 {
 	int i;
-	unsigned long int num1, num2, next, sum;
+	unsigned long f1 = 0, f2 = 1, res;
+	unsigned long f1_h1, f1_h2, f2_h1, f2_h2;
+	unsigned long h1, h2;
 
-	num1 = 1;
-	num2 = 2;
-	sum = 0;
-
-	for (i = 1; i <= 33; i++)
+	for (i = 0; i < 92; i++)
 	{
-		if (num1 < 4000000 && (num1 % 2) == 0)
-		{
-			sum = sum + num1;
-		}
-		next = num1 + num2;
-		num1 = num2;
-		num2 = next;
+		res = f1 + f2;
+		printf("%lu, ", res);
+		f1 = f2;
+		f2 = res;
 	}
-
-	printf("%lu\n", sum);
-
+	f1_h1 = f1 / 10000000000;
+	f2_h1 = f2 / 10000000000;
+	f1_h2 = f1 % 10000000000;
+	f2_h2 = f2 % 10000000000;
+	for (i = 93; i < 99; i++)
+	{
+		h1 = f1_h1 + f2_h1;
+		h2 = f1_h2 + f2_h2;
+		if  (f1_h2 + f2_h2 > 9999999999)
+		{
+			h1 += 1;
+			h2 %= 10000000000;
+		}
+		printf("%lu%lu", h1, h2);
+		if (i != 98)
+			printf(", ");
+		f1_h1 = f2_h1;
+		f1_h2 = f2_h2;
+		f2_h1 = h1;
+		f2_h2 = h2;
+	}
+	printf("\n");
 	return (0);
 }
