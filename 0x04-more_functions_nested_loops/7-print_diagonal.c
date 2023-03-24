3b47094fc0c88ef8eb9e72723f65163cf8776e3c
Fatifonon <fatifnon0@example.com>
2023-03-24 16:56:05 -0700

    .

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,11 +15,11 @@ void print_diagonal(int n)
 		int i;
 		int j;
 
-		for (i = 1; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < n; j++)
 			{
-				if (j == i)
+				if (j -- i)
 					_putchar('\\');
 				else if (j < i)
 					_putchar(' ');
