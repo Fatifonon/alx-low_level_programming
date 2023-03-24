c8f7463c90d14bb58b63eb70adaab18853470191
Fatifonon <fatifnon0@example.com>
2023-03-24 14:14:15 -0700

    .

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers from 0 to 9 followed by a new line
+ * print_numbers - prints 0 - 9
  * Return: void
  */
 void print_numbers(void)
@@ -10,7 +10,7 @@ void print_numbers(void)
 
 	for (n = '0'; n <= '9'; n++)
 	{
-			_putchar(n);
+		_putchar(n);
 	}
 	_putchar('\n');
 }
