6627aacbb5ce2cc4075823831086a991d868d69c
Fatifonon <fatifnon0@example.com>
2023-03-24 14:56:12 -0700

    .

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,9 +8,9 @@ void more_numbers(void)
 {
 	char n; m
 
-	for (n = '1'; n <= 10; n++)
+	for (n = 1; n <= 10; n++)
 	{
-		for (m = '0'; m <= 14; m++)
+		for (m = 0; m <= 14; m++)
 		{
 			if (m >= 10)
 				_putchar('1');
