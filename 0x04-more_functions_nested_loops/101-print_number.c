86a13d0c5becc85a59f9fd3c9318440c7f7b8b90
Fatifonon <fatifnon0@example.com>
2023-03-24 18:29:37 -0700

    .

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,20 +5,18 @@
 */
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int k = n;
 
 	if (n < 0)
 	{
-		n1 = -n;
+		n * = -1;
+		k = n;
 		_putchar('-');
 	}
-	else
+	k /= 10;
+	if (k != 0)
 	{
-		n1 = n;
-	}
-	if (n1 / 10)
-	{
-		print_number(n1 / 10);
-	}
-	_putchar((n1 % 10) + '0');
+		print_number(k);
+		_putchar((unsigned int)n % 10 + '0');
+	}	
 }
