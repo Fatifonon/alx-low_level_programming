2c2b8500d26c7b6d28e3d3861f4661d46fa20ede
Fatifonon <fatifnon0@example.com>
2023-03-24 18:18:00 -0700

    .

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,26 +1,24 @@
 #include "main.h"
-
 /**
- * print_number - prints an integer
- * @n: integer to be printed
- */
+* print_number - prints an integer
+* @n: integer to be printed
+*/
 void print_number(int n)
 {
-	unsigned int num;
+	unsigned int n1;
 
 	if (n < 0)
 	{
-		num = -n;
+		n1 = -n;
 		_putchar('-');
-	} else
-	{
-		num = n;
 	}
-
-	if (num / 10)
+	else
 	{
-		print_number(num / 10);
+		n1 = n;
 	}
-
-	_putchar((num % 10) + '0');
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
