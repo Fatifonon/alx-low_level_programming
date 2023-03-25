5c0f8a8669e63291476af6a2511eda9af22a47f6
Fatifonon <fatifnon0@example.com>
2023-03-24 17:58:38 -0700

    .

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,7 +3,7 @@
 /**
  * main - prints the numbers from 1 to 100, followed by a new line
  * But for multiples of three print Fizz instead of the number
- * and for the multiples of five print fizzBuzz
+ * and for the multiples of five print Buzz
  * Return: Always 0 (Success)
  */
 int main(void)
