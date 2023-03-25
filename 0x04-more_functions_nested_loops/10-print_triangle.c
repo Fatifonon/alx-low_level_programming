eadcd2346a4cfeb1111407c1cf1d73aad017ae82
Fatifonon <fatifnon0@example.com>
2023-03-24 17:26:35 -0700

    .

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,13 +15,17 @@ void print_triangle(int size)
 		int i;
 		int j;
 
-		for (i = 0; i < size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = 0; j < size; j++)
+			for (j = i; j < size; j++)
 			{
-			_putchar('#');
+				_putchar('');
 			}
-		_putchar('\n');
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
 	}
 }
