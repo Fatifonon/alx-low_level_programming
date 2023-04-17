3da04a203f76a14c951f20002c9deabd3c2b935f
Fatifonon <fatifnon0@example.com>
2023-04-17 11:39:14 -0700

    .

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,8 +11,8 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		d->name = "nil";
+		d->name = "(nil)";
 	if (d->owner == NULL)
-		d->owner = "nil";
+		d->owner = "(nil)";
 	printf("name: %s\nage: %f\nowner: %s\n", d->name, d->age, d->owner);
 }
